b8d176a302377fae2e66e9a292bc8a000ac38ccf
antonieoqq <55801684@qq.com>
2025-12-17 09:37:45 +0800

    4.6.0 开发 readonly ref struct RentedArray<T>

---
+++ XDFLib/Collections/RentedArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Buffers;
+
+namespace XDFLib.Collections
+{
+    /// <summary> A lightweight struct that rents an array from the ArrayPool and returns it on Dispose. </summary>
+    public readonly ref struct RentedArray<T>
+    {
+        public readonly int Length;
+        public Span<T> Span => _array.AsSpan(0, Length);
+
+        private readonly ArrayPool<T> _pool;
+        private readonly T[] _array;
+
+        public RentedArray(int minLength, ArrayPool<T>? pool = null)
+        {
+            _pool = pool ?? ArrayPool<T>.Shared;
+            _array = _pool.Rent(minLength);
+            Length = minLength;
+            Array.Clear(_array, 0, minLength);
+        }
+
+        public void CopyTo(T[] destination, int start)
+        {
+            Array.Copy(_array, 0, destination, start, Length);
+        }
+
+        public void CopyTo(T[] destination, int start, int count)
+        {
+            Array.Copy(_array, 0, destination, start, count);
+        }
+
+        public void Dispose()
+        {
+            Array.Clear(_array, 0, Length);
+            _pool.Return(_array);
+        }
+    }
+}
