6d90f98ad2d0d286d9d8334d2aa9f8d8ee4d6d85
antonieoqq <55801684@qq.com>
2026-03-26 11:26:30 +0800

    hotfix

---
+++ XDFLib/Collections/AList_Statics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace XDFLib.Collections
+{
+    public static class AList
+    {
+        public static AList<T> CreateFrom<T>(ReadOnlySpan<T> array, bool filterNull = true)
+        {
+            var result = new AList<T>(array.Length);
+            foreach (var item in array)
+            {
+                if (filterNull && item == null) continue;
+                result.Add(item);
+            }
+            return result;
+        }
+    }
+}
